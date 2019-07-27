d8e9d92fddc9247b471bf2b9de0b9025c868d032
VivekP <>
2019-07-27 11:51:36 +0530

    updating.

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -2,8 +2,8 @@ package utility;
 
 public class Constant {
 	    public static final String URL = "http://www.onlinestore.toolsqa.com";
-	    public static final String Username = "testuser_1";
-	    public static final String Password ="Test@123";
+	    public static final String Username = "testuser_12";
+	    public static final String Password ="Test@1232";
 		public static final String Path_TestData = "D://ToolsQA//OnlineStore//src//testData//";
 		public static final String File_TestData = "TestData.xlsx";
 		
